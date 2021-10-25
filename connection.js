e19cf0d950a3cc13fae5ef1357860909efab8381
Miguel Pascual <miguel@yonder.com>
2021-10-24 20:26:19 -0700

    Added mongoclient fn

--- connection.js
+++ connection.js
@@ -1,3 +1,9 @@
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const URI = process.env.MONGO_URI;
+
+async function main(callback) {
+    const URI = process.env['MONGO_URI']; // Declare MONGO_URI in your .env file
+    const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+}
