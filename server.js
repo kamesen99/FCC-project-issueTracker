218def8172c46f707e527fb8f38a0a3c77b99dd8
kamesen99 <kamesen@gmail.com>
2021-10-27 16:37:53 -0700

    Added express call

--- server.js
+++ server.js
@@ -6,3 +6,11 @@ const expect      = require('chai').expect;
 const cors        = require('cors');
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
+const myDB = require('./connection');
+
+let app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(cors({origin: '*'})); //For FCC testing purposes only
