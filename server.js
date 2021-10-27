7d7c348a7cfaa8b31aa2714ba8778ecd5d4df2ee
kamesen99 <kamesen@gmail.com>
2021-10-27 16:37:19 -0700

    Added additional reqs

--- server.js
+++ server.js
@@ -2,3 +2,7 @@
 require('dotenv').config();
 const express     = require('express');
 const bodyParser  = require('body-parser');
+const expect      = require('chai').expect;
+const cors        = require('cors');
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
